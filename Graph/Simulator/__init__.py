d10d22b33d172385d65740fef535f855f7dba4df
Lucas Pereira Reis <lprs.eng@uea.edu.br>
2018-11-17 16:17:18 -0400

    test: debugging yen's algorithm

--- Graph/Simulator/__init__.py
+++ Graph/Simulator/__init__.py
@@ -7,6 +7,7 @@ from PyQt5.QtGui import QIntValidator
 class SimulatorDialog(QDialog):
     NumGridRows = 3
     NumButtons = 4
+    
 
     def __init__(self, parentApp, n, used_label_edges):
         super(SimulatorDialog, self).__init__()
@@ -100,20 +101,27 @@ class SimulatorDialog(QDialog):
                 continue   
 
             s_d = (s,d)
+            
+
             if s_d not in sorteados_s_d:
                 sorteados_s_d.append(s_d)
-                rotas[s_d] = []           
+                rotas[s_d] = []
+                #print(s_d)
+                #print(rotas[s_d])                          
             
             if(sources[s] is None):
                 graph.dijkstra_sssp(s)
                 sources[s] = graph.vertexes
+                #print("sdi = ",sources[s])
                 path = graph.path(d, sources[s])
                 rota = [int(vertex) for vertex in path.split()]
             
-            if rota not in rotas[s_d]:
-                rotas[s_d] = rotas[s_d].append(rota)
+                if rota not in rotas[s_d]:
+                    #print('chegou aqui')
+                    rotas[s_d].append(rota)
+                    #print(rotas[s_d])
 
-                self.currengtPath = []
+                self.currentPath = []
         
                 f.write('Simulacao ' + str(countSimulations - leftSimulations + 1) + ':\n')
                 f.write('Origem: ' + str((self.index_to_vertex[s].label.text())) + '\n')
@@ -123,12 +131,17 @@ class SimulatorDialog(QDialog):
 
                 #self.firstFit(f)
             else:
-             
-                sources[s] = graph.ksp_yen(s, d, sources[s])[0]['path']
+                yen_result = graph.ksp_yen(s, d, sources[s])[0]
+                sources[s], distance = yen_result['path'], yen_result['cost']
 
-                path = graph.path(d, sources[s])
+                #print("syen = ",sources[s])
+
+                #path = graph.path(d, sources[s])
+                path = self.path_of_vertexes(sources[s]) 
                 rota = [int(vertex) for vertex in path.split()]
-            
+                #print(f'ROTA: {rota}')
+                print(f'ROTA CRIADA: {rota}')
+                print(f'ROTAS: {rotas[s_d]}')
                 if rota in rotas[s_d]:
                     continue
         
@@ -137,9 +150,11 @@ class SimulatorDialog(QDialog):
                     f.write('Simulacao ' + str(countSimulations - leftSimulations + 1) + ':\n')
                     f.write('Origem: ' + str((self.index_to_vertex[s].label.text())) + '\n')
                     f.write('Destino: ' + str((self.index_to_vertex[d].label.text())) + '\n')
-                    f.write('Caminho: ' + str(graph.path_to(d, sources[s], self.handleVertices)) + '\n')
-                    f.write('Distancia: ' + str(sources[s][d].distance) + ' km\n'+'\n')
-                    rotas[s_d] = rotas[s_d].append(rota)
+                    #f.write('Caminho: ' + str(graph.path_to(d, sources[s], self.handleVertices)) + '\n')
+                    f.write('Caminho: ' + path + '\n')
+                    #f.write('Distancia: ' + str(sources[s][d].distance) + ' km\n'+'\n')
+                    f.write('Distancia: ' + str(distance) + 'km\n' + '\n')
+                    rotas[s_d].append(rota)
 
                 else:
                     f.write('Simulacao ' + str(countSimulations - leftSimulations + 1) + ':\n')
@@ -161,6 +176,17 @@ class SimulatorDialog(QDialog):
             self.parentApp.showNewMessageDialog('Arquivo do resultado da Simulação criado:  \"' + fileSimulationName + '\"')
         except:
             self.parentApp.showNewMessageDialog('Something went wrong')
+
+    def path_of_vertexes(self, vertex_list):
+        path = ""
+        print(vertex_list)
+        for vertex in vertex_list:
+            path += vertex.label + ' '
+
+        path.rstrip()
+
+        return path
+
 
             
     def handleVertices(self, u):
